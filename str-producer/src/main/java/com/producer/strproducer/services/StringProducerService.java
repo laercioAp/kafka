68fa95b4d83a119792755bed9f57a12a7338f32f
Laercio Aparecido Pedroso <laerciogu00@gmail.com>
2023-06-27 21:09:23 -0300

    fix: criando endpoint para criacao de msgs no kafka

---
+++ str-producer/src/main/java/com/producer/strproducer/services/StringProducerService.java
@@ -0,0 +1,22 @@
+package com.producer.strproducer.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        try {
+            kafkaTemplate.send("str-topic", message);
+            log.info("Mensagem enviada com suceso{}", message);
+        } catch (Exception ex) {
+            log.error("Error ao enviar a message ao topico", ex);
+        }
+    }
+}
