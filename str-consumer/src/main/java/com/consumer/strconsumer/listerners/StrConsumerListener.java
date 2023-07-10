09eb57892f96bfe7aa6309863ba77b8f37225de0
Laercio Aparecido Pedroso <laerciogu00@gmail.com>
2023-07-10 20:19:50 -0300

    error handler

--- str-consumer/src/main/java/com/consumer/strconsumer/listerners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/consumer/strconsumer/listerners/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package com.consumer.strconsumer.listerners;
 
 import com.consumer.strconsumer.custom.StrConsumerCustomListener;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListener {
 
+    @SneakyThrows
     @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("CREATE ::: Mensagem recebida {}", message);
+        throw new IllegalArgumentException("EXCEPTION ...");
     }
 
     @StrConsumerCustomListener(groupId = "group-1")
